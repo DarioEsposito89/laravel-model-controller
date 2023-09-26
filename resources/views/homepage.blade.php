9305fd8db12478e181d6d0950943f1d04ef6d7d8
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-26 18:07:45 +0200

    create card dove stampare i film

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,25 @@
 @extends("layouts.public")
 
 @section("content")
-    <h1>Hello World struttura base</h1>
+<div class="container">
+    <div class="row">
+            @forelse ($movies as $movie)
+            <div class="card me-5 mb-5" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie['title'] }}</h5>
+                    <h6 class="">{{ $movie['original_title'] }}</h5>
+                    <p class="card-text"> {{ $movie['nationality'] }}</p>
+                    <span>
+                        {{  $movie['date'] }}
+                    </span>
+                    <span>
+                        {{  $movie['vote'] }}
+                    </span>
+                </div>
+            </div>
+            @empty
+                <p>Mi disp non ci sono film da mostrare</p>
+            @endforelse
+    </div>
+</div>
 @endsection
